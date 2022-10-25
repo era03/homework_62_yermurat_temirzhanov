2f8d691953a1dbea42be02024960c4da5e2d1ff9
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-10-25 19:24:54 +0600

    Добавлены разрешения для пользователей

--- to_do_list/webapp/views/projects.py
+++ to_do_list/webapp/views/projects.py
@@ -1,10 +1,19 @@
+from tokenize import group
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from webapp.models import Projects, Tasks
 from django.urls import reverse, reverse_lazy
 from webapp.forms import ProjectForm, TaskForm, AddUserToProjectForm
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
+
+
+class GroupPermissions(UserPassesTestMixin):
+
+    groups = []
+
+    def test_func(self):
+        return self.request.user.groups.filter(name__in=self.groups).exists()
 
 
 class ProjectsView(ListView):
@@ -20,19 +29,21 @@ class ProjectDetailView(DetailView):
     context_object_name = 'project'
 
     
-class ProjectCreateView(LoginRequiredMixin, CreateView):
+class ProjectCreateView(GroupPermissions, LoginRequiredMixin, CreateView):
     template_name = 'project_create.html'
     form_class = ProjectForm
     model = Projects
+    groups = ['Project Manager']
 
     def get_success_url(self):
         return reverse('projects')
 
 
-class ProjectTaskCreateView(LoginRequiredMixin, CreateView):
+class ProjectTaskCreateView(GroupPermissions, LoginRequiredMixin, CreateView):
     model = Tasks
     form_class = TaskForm
     template_name = 'task_create.html'
+    groups = ['Project Manager', 'Developer']
 
     def form_valid(self, form):
         project = get_object_or_404(Projects, pk=self.kwargs.get('pk'))
@@ -43,17 +54,19 @@ class ProjectTaskCreateView(LoginRequiredMixin, CreateView):
         return reverse('projects')
 
 
-class ProjectDeleteView(LoginRequiredMixin, DeleteView):
+class ProjectDeleteView(GroupPermissions, LoginRequiredMixin, DeleteView):
     template_name = 'article_confirm_delete.html'
     model = Projects
     success_url = reverse_lazy('index')
+    groups = ['Project Manager']
 
 
-class AddUserToProjectView(UpdateView):
+class AddUserToProjectView(GroupPermissions, LoginRequiredMixin, UpdateView):
     model = Projects
     form_class = AddUserToProjectForm
     template_name = 'add_user_to_project.html'
     context_object_name = 'project'
+    groups = ['Project Manager', 'Team Lead']
 
     def get_success_url(self):
         return reverse('projects')
--- to_do_list/webapp/views/tasks.py
+++ to_do_list/webapp/views/tasks.py
@@ -7,33 +7,42 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
+class GroupPermissions:
+
+    def test_func(self):
+        return self.request.user.groups.filter(name__in=self.groups).exists()
+
+
 class TaskDetailView(DetailView):
     template_name = 'task_detail.html'
     model = Tasks
     context_object_name = 'task'
 
 
-class TaskCreateView(LoginRequiredMixin, CreateView):
+class TaskCreateView(GroupPermissions, LoginRequiredMixin, CreateView):
     template_name = 'task_create.html'
     form_class = TaskForm
     model = Tasks
+    groups = ['Project Manager', 'Team Lead', 'Developer']
 
     def get_success_url(self):
         return reverse('index')
 
 
-class TaskUpdateView(LoginRequiredMixin, UpdateView):
+class TaskUpdateView(GroupPermissions, LoginRequiredMixin, UpdateView):
     template_name = 'task_edit.html'
     form_class = TaskForm
     model = Tasks
     context_object_name = 'task'
+    groups = ['Project Manager', 'Team Lead', 'Developer']
 
     def get_success_url(self):
         return reverse('index')
 
 
-class TaskDeleteView(LoginRequiredMixin, DeleteView):
+class TaskDeleteView(GroupPermissions, LoginRequiredMixin, DeleteView):
     template_name = 'task_confirm_delete.html'
     model = Tasks
     success_url = reverse_lazy('index')
     context_object_name = 'task'
+    groups = ['Project Manager', 'Team Lead']
